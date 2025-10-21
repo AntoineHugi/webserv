a34549640897816d3a62f560bc134391e27f3ad5
Antoine Hugi 42 <ahugi@student.42berlin.de>
2025-10-21 13:01:35 +0200

    refactored parsing

--- srcs/server.cpp
+++ srcs/server.cpp
@@ -1,6 +1,6 @@
 #include "server.hpp"
 
-Server::Server():_name(""), _port(-1), _host(""), _root(""), _index(), _error_page(""), _client_max_body_size(-1) {}
+Server::Server():_name(""), _port(-1), _host(""), _root(""), _index(), _error_page(""), _client_max_body_size(-1), _CGI() {}
 
 Server::Server(const Server& other) 
 {
@@ -11,6 +11,7 @@ Server::Server(const Server& other)
 	this->_index = other._index;
 	this->_error_page = other._error_page;
 	this->_client_max_body_size = other._client_max_body_size;
+	this->_CGI = other._CGI;
 }
 
 Server& Server::operator=(const Server& other)
@@ -24,13 +25,14 @@ Server& Server::operator=(const Server& other)
 		this->_index = other._index;
 		this->_error_page = other._error_page;
 		this->_client_max_body_size = other._client_max_body_size;
+		this->_CGI = other._CGI;
 	}
 	return (*this);
 }
 
 Server::~Server() {}
 
-void	Server::set_name(std::string name) { this->_name = name; }
+void	Server::set_name(std::string& name) { this->_name = name; }
 
 std::string	Server::get_name() { return (this->_name); }
 
@@ -38,11 +40,11 @@ void	Server::set_port(int port) { this->_port = port; }
 
 int	Server::get_port() { return (this->_port); }
 
-void	Server::set_host(std::string host) { this->_host = host; }
+void	Server::set_host(std::string& host) { this->_host = host; }
 
 std::string	Server::get_host() { return (this->_host); }
 
-void	Server::set_root(std::string root) { this->_root = root; }
+void	Server::set_root(std::string& root) { this->_root = root; }
 
 std::string	Server::get_root() { return (this->_root); }
 
@@ -50,10 +52,14 @@ void	Server::set_index(std::vector <std::string> index) { this->_index = index; 
 
 std::vector <std::string>	Server::get_index() { return (this->_index); }
 
-void	Server::set_error_page(std::string page) { this->_error_page = page; }
+void	Server::set_error_page(std::string& page) { this->_error_page = page; }
 
 std::string	Server::get_error_page() { return (this->_error_page); }
 
 void	Server::set_client_max_body_size(int max) { this->_client_max_body_size = max; }
 
 int	Server::get_client_max_body_size() { return (this->_client_max_body_size); }
+
+void	Server::insert_CGI(std::string& key, std::map<std::string, std::string> value) { this->_CGI.insert(std::make_pair(key, value)); }
+
+std::map <std::string, std::map<std::string, std::string> >	Server::get_CGI() { return (this->_CGI); }
