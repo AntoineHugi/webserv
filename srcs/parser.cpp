0f60860acb54bf2f9a95a8d6b88552a957635498
Antoine Hugi 42 <ahugi@student.42berlin.de>
2025-10-21 13:17:19 +0200

    expanded error handling

--- srcs/parser.cpp
+++ srcs/parser.cpp
@@ -37,7 +37,7 @@ bool	Parser::open_config_file(char *arg, Service* service)
 
 	if (filename.empty())
 	{
-		std::cout << "Error: no file name" << std::endl;
+		std::cout << "Config file error: no file name" << std::endl;
 		return (false);
 	}
 	std::ifstream file(filename.c_str());
@@ -47,7 +47,10 @@ bool	Parser::open_config_file(char *arg, Service* service)
 			config.append(line);
 		file.close();
 		if (config == "")
+		{
+			std::cout << "Config file error: file empty" << std::endl;
 			return (false);
+		}
 		else
 		{
 			if (!parse_config_file(config, service))
@@ -58,7 +61,7 @@ bool	Parser::open_config_file(char *arg, Service* service)
 	}
 	else
 	{
-		std::cout << "Error: couldn't open file" << std::endl;
+		std::cout << "Config file error: couldn't open file" << std::endl;
 		return (false);
 	}
 }
@@ -72,7 +75,11 @@ bool	Parser::parse_config_file(std::string config, Service* service)
 	{
 		++i;
 		if (i >= tokens.size() || tokens[i] != "{")
+		{
+			--i;
+			std::cout << "Config file error: expecting '{' after " << tokens[i] << std::endl;
 			return (false);
+		}
 		++i;
 		Server server;
 		while (i < tokens.size() && tokens[i] != "}")
@@ -81,7 +88,11 @@ bool	Parser::parse_config_file(std::string config, Service* service)
 			{
 				++i;
 				if (i >= tokens.size() || tokens[i] == ";")
+				{
+					--i;
+					std::cout << "Config file error: unexpected element after " << tokens[i] << std::endl;
 					return (false);
+				}
 				if (!parse_location(&server, tokens, &i))
 					return (false);
 			}
@@ -90,7 +101,11 @@ bool	Parser::parse_config_file(std::string config, Service* service)
 				std::string key = tokens[i];
 				++i;
 				if (tokens[i] == ";")
+				{
+					--i;
+					std::cout << "Config file error: unexpected element after " << tokens[i] << std::endl;
 					return (false);
+				}
 				std::vector <std::string> values;
 				while (tokens[i] != ";" && i < tokens.size())
 				{
@@ -98,7 +113,11 @@ bool	Parser::parse_config_file(std::string config, Service* service)
 					++i;
 				}
 				if (i >= tokens.size())
+				{
+					--i;
+					std::cout << "Config file error: unexpected element after " << tokens[i] << std::endl;
 					return (false);
+				}
 				if (!assign_vector_keyval(&server, key, values))
 					return (false);
 				++i;
@@ -108,18 +127,30 @@ bool	Parser::parse_config_file(std::string config, Service* service)
 				std::string key = tokens[i];
 				++i;
 				if (i >= tokens.size() || tokens[i] == ";")
+				{
+					--i;
+					std::cout << "Config file error: unexpected element after " << tokens[i] << std::endl;
 					return (false);
+				}
 				std::string value = tokens[i];
 				++i;
 				if (i >= tokens.size() || tokens[i] != ";")
+				{
+					--i;
+					std::cout << "Config file error: expected ';' after " << tokens[i] << std::endl;
 					return (false);
+				}
 				if (!assign_single_keyval(&server, key, value))
 					return (false);
 				++i;
 			}
 		}
 		if (tokens[i] != "}")
+		{
+			--i;
+			std::cout << "Config file error: expecting '}' after " << tokens[i] << std::endl;
 			return (false);
+		}
 		++i;
 		if (!check_server(&server))
 			return (false);
@@ -140,7 +171,11 @@ bool Parser::parse_location(Server* server, std::vector <std::string> tokens, si
 		++(*i);
 	}
 	if (*i >= tokens.size())
+	{
+		--(*i);
+		std::cout << "Config file error: unexpected element after " << tokens[*i] << std::endl;
 		return (false);
+	}
 	std::string cgi_key;
 	if (case_sensitive)
 		cgi_key = tokens[*i];
@@ -152,26 +187,51 @@ bool Parser::parse_location(Server* server, std::vector <std::string> tokens, si
 	}
 	++(*i);
 	if (*i >= tokens.size() || tokens[*i] != "{")
+	{
+		--(*i);
+		std::cout << "Config file error: expecting '{' after " << tokens[*i] << std::endl;
 		return (false);
+	}
 	++(*i);
 	std::map<std::string, std::string> cgi_map;
 	while (*i < tokens.size() && tokens[*i] != "}")
 	{
 		if (tokens[*i] == ";" || tokens[*i] == "{")
+		{
+			--(*i);
+			std::cout << "Config file error: unexpected element after " << tokens[*i] << std::endl;
 			return (false);
+		}
 		std::string key = tokens[*i];
 		++(*i);
 		if (*i >= tokens.size() || tokens[*i] == ";" || tokens[*i] == "{")
+		{
+			--(*i);
+			std::cout << "Config file error: unexpected element after " << tokens[*i] << std::endl;
 			return (false);
+		}
 		std::string value = tokens[*i];
 		++(*i);
 		if (*i >= tokens.size() || tokens[*i] != ";")
+		{
+			--(*i);
+			std::cout << "Config file error: expecting ';' after " << tokens[*i] << std::endl;
 			return (false);
+		}
 		cgi_map.insert(std::make_pair(key, value));
 			++(*i);
 	}
-	if (*i >= tokens.size() || cgi_map.size() == 0)
+	if (*i >= tokens.size() || tokens[*i] != "}")
+	{
+		--(*i);
+		std::cout << "Config file error: expecting '}' after " << tokens[*i] << std::endl;
 		return (false);
+	}
+	if (cgi_map.size() == 0)
+	{
+		std::cout << "Config file error: CGI mapping empty " << std::endl;
+		return (false);
+	}
 	server->insert_CGI(cgi_key, cgi_map);
 	return (true);
 }
@@ -190,36 +250,55 @@ bool Parser::assign_single_keyval(Server* server, std::string& key, std::string&
 	{
 		case 0:
 			if (!server->get_name().empty())
+			{
+				std::cout << "Config file error: duplicate server name" << std::endl;
 				return (false);
+			}
 			server->set_name(value);
 			break ;
 		case 1:
 			if (server->get_port() != -1)
+			{
+				std::cout << "Config file error: duplicate port" << std::endl;
 				return (false);
+			}
 			server->set_port(atoi(value.c_str()));
 			break ;
 		case 2:
 			if (!server->get_host().empty())
+			{
+				std::cout << "Config file error: duplicate host" << std::endl;
 				return (false);
+			}
 			server->set_host(value);
 			break ;
 		case 3:
 			if (!server->get_root().empty())
+			{
+				std::cout << "Config file error: duplicate root" << std::endl;
 				return (false);
+			}
 			server->set_root(value);
 			break ;
 		case 4:
 			if (!server->get_error_page().empty())
+			{
+				std::cout << "Config file error: duplicate error page" << std::endl;
 				return (false);
+			}
 			server->set_error_page(value);
 			break ;
 		case 5:
 			if (server->get_client_max_body_size() != -1)
+			{
+				std::cout << "Config file error: duplicate max body size" << std::endl;
 				return (false);
+			}
 			server->set_client_max_body_size(atoi(value.c_str()));
 			break;
 		default:
-			return (false);
+				std::cout << "Config file error: field not valid" << std::endl;
+				return (false);
 	}
 	return (true);
 }
@@ -238,7 +317,10 @@ bool Parser::assign_vector_keyval(Server* server, std::string& key, std::vector 
 	{
 		case 0:
 			if (!server->get_index().empty())
+			{
+				std::cout << "Config file error: duplicate index" << std::endl;
 				return (false);
+			}
 			server->set_index(values);
 			break ;
 		default:
@@ -250,18 +332,39 @@ bool Parser::assign_vector_keyval(Server* server, std::string& key, std::vector 
 bool Parser::check_server(Server* server)
 {
 	if (server->get_name().empty())
+	{
+		std::cout << "Config file error: server name missing" << std::endl;
 		return (false);
+	}
 	if (server->get_port() == -1)
+	{
+		std::cout << "Config file error: port missing" << std::endl;
 		return (false);
+	}
 	if (server->get_host().empty())
+	{
+		std::cout << "Config file error: host missing" << std::endl;
 		return (false);
+	}
 	if (server->get_root().empty())
+	{
+		std::cout << "Config file error: root missing" << std::endl;
 		return (false);
+	}
 	if (server->get_index().empty())
+	{
+		std::cout << "Config file error: index missing" << std::endl;
 		return (false);
+	}
 	if (server->get_error_page().empty())
+	{
+		std::cout << "Config file error: error page missing" << std::endl;
 		return (false);
+	}
 	if (server->get_client_max_body_size() == -1)
+	{
+		std::cout << "Config file error: max client body size missing" << std::endl;
 		return (false);
+	}
 	return (true);
 }
